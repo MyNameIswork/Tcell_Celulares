6d4017c78fd6f62213f5f2f63443657dcb18c67d
Vittor <65642106+MyNameIswork@users.noreply.github.com>
2026-08-05 01:09:42 -0300

    Update corsConfig.ts

--- server/src/config/corsConfig.ts
+++ server/src/config/corsConfig.ts
@@ -5,7 +5,8 @@ import cors from "cors";
 const routesAccess = process.env.FRONT_URL as string;
 
 const corsConfig = cors({
-    origin: process.env.NODE_ENV == "development" ? "*" : routesAccess,
+    //origin: process.env.NODE_ENV == "development" ? "*" : routesAccess,
+    origin: true,
     credentials: true,
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
