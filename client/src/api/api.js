7f1f6130d865cdf6c5a6a5940ce4b503717f4ea7
Vittor <65642106+MyNameIswork@users.noreply.github.com>
2026-08-05 00:38:26 -0300

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -4,7 +4,7 @@ console.log("Route: ", import.meta.env.VITE_API_BASE_URL);
 //https://tcellecommerce-production.up.railway.app
 const api = axios.create({
   //baseURL: import.meta.env.VITE_API_BASE_URL, // URL base do .env
-  baseURL: "https://tcellcelulares-production-03b1.up.railway.app",
+  baseURL: "https://tcellecommerce-production.up.railway.app",
 });
 
 export default api;
