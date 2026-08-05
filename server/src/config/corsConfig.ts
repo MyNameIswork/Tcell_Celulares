6266c135bc126a5bed7fa20f708fbcc557b981b9
Vittor <65642106+MyNameIswork@users.noreply.github.com>
2026-08-05 01:11:14 -0300

    Update corsConfig.ts

--- server/src/config/corsConfig.ts
+++ server/src/config/corsConfig.ts
@@ -6,7 +6,11 @@ const routesAccess = process.env.FRONT_URL as string;
 
 const corsConfig = cors({
     //origin: process.env.NODE_ENV == "development" ? "*" : routesAccess,
-    origin: true,
+    origin: [
+        "https://tcellcelulares.com",
+        "tcellcelulares.com",
+        "https://www.tcellcelulares.com",
+        "https://tcellecommerce-production.up.railway.app"],
     credentials: true,
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
