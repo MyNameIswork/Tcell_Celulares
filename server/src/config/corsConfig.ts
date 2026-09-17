8029647f22c15c62e66c66f7df8f9d70010d6132
Vittor <65642106+MyNameIswork@users.noreply.github.com>
2026-09-17 12:31:49 -0300

    Update corsConfig.ts

--- server/src/config/corsConfig.ts
+++ server/src/config/corsConfig.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 //const routesAccess = process.env.FRONT_URL as string;
 
 const corsConfig = cors({
-    origin: true,
+    origin: "*",
     credentials: true,
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
