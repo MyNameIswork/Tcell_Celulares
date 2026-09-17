1e97d7f64861e17d4da817edda2913be012d04a2
Vittor <65642106+MyNameIswork@users.noreply.github.com>
2026-09-17 12:55:55 -0300

    Update server.ts

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -25,7 +25,7 @@ app.use("/", (req: Request, _, next: NextFunction) => {
     next();
 });
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
     console.error("Erro não tratado:", err);
     res.status(500).json({ message: "Erro interno no servidor" });
 });
