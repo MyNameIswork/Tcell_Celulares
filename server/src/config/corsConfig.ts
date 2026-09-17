ddb5d50a991ecd39ef9fdfd36c7714a274d0b874
Vittor <65642106+MyNameIswork@users.noreply.github.com>
2026-09-17 12:39:44 -0300

    Update corsConfig.ts

--- server/src/config/corsConfig.ts
+++ server/src/config/corsConfig.ts
@@ -1,10 +1,19 @@
-//import "dotenv/config";
+import "dotenv/config";
 import cors from "cors";
 
-//const routesAccess = process.env.FRONT_URL as string;
+const allowedOrigins = [
+    "https://www.tcellcelulares.com",
+    "http://localhost:3000", // se usar em dev
+];
 
 const corsConfig = cors({
-    origin: "*",
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true,
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
